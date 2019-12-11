096b0dfcc5585bfff7bcf3126df88c2410147e5d
Manninen Olli <s1900562@edu.bc.fi>
2019-12-11 10:32:56 +0200

    second commit

--- index.js
+++ index.js
@@ -1,32 +1,104 @@
-console.log('Hello')
 
-const http = require('http')
+const express = require("express");
+const app = express();
+let morgan = require("morgan");
+const cors = require('cors')
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
+const bodyParser = require("body-parser");
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(requestLogger);
+app.use(morgan("tiny"));
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "0406653321"
+  },
+  {
+    id: 2,
+    name: "Olli Manninen",
+    number: "0506976455"
+  },
+  {
+    id: 3,
+    name: "Jussi Pupunen",
+    number: "0401234567"
+  },
+  {
+    id: 4,
+    name: "Jorma Uotila",
+    number: "0449876543"
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`<h2>Contacts in Phonebook: ${persons.length}</h2> ${new Date()}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  console.log(id, typeof id, person.id, typeof person.id);
+  person ? res.json(person) : res.status(404).end();
+});
+
+app.delete("api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log("poistettava id: ", id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("api/persons/", (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number missing"
+    });
+  }
+  persons.map(person => {
+    if (person.name === body.name) {
+      return res.status(400).json({
+        error: "name is already in Phonebook"
+      });
     }
-  ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
-  
-  const port = 3001
-  app.listen(port)
-  console.log(`Server running on port ${port}`)
+  });
+
+  console.log(req.headers);
+  const id = Math.floor(Math.random() * 1000);
+  const personToAdd = { ...body, id: id };
+  persons.concat(personToAdd);
+  res.json(persons);
+  // res.send(`<h2>New contact added: ${personToAdd.name} </h2>`);
+});
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
